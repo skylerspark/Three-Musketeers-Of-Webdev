47ec575dec66ecb06638b1bac1d2ece5d0fd7081
skylerspark <44554976+skylerspark@users.noreply.github.com>
2020-01-01 16:05:42 -0500

    Moving the Repo to Functional Workflow

--- Source/script.js
+++ Source/script.js
@@ -3,3 +3,12 @@ function link(u) {
 }
 
 document.querySelector('.sideNav').style.height = document.querySelector(".content").style.height;
+
+var toggler = document.getElementsByClassName("caret");
+
+for (var i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+    });
+}
